5ef00f261094e36a3437e37adc61a84cfa188f41
AlHobb <118611357+alkopotov@users.noreply.github.com>
2024-05-02 13:46:36 +0300

    Not Found Page

--- src/app/pages/not-found-page/not-found-page.component.ts
+++ src/app/pages/not-found-page/not-found-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './not-found-page.component.html',
   styleUrl: './not-found-page.component.css'
 })
