b261482414189e1f549e2a3767820a8d3fd00c18
AlHobb <118611357+alkopotov@users.noreply.github.com>
2024-05-03 11:16:49 +0300

    Fonts added

--- src/app/components/footer-elem/footer-elem.component.ts
+++ src/app/components/footer-elem/footer-elem.component.ts
@@ -1,12 +1,71 @@
 import { Component } from '@angular/core';
+import { MainLogoComponent } from '../svg_components/main-logo/main-logo.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer-elem',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MainLogoComponent],
   templateUrl: './footer-elem.component.html',
   styleUrl: './footer-elem.component.css'
 })
 export class FooterElemComponent {
 
+  public routerLinks: any[] = [
+    {
+      link: '/guarantee',
+      title: 'Гарантия'
+    },
+    {
+      link: '/returns',
+      title: 'Политика возврата'
+    },
+    {
+      link: '/credit',
+      title: 'Кредит'
+    },
+    {
+      link: '/contacts',
+      title: 'Контакты'
+    },
+    {
+      link: '/delivery',
+      title: 'Доставка и оплата'
+    },
+    {
+      link: '/comments',
+      title: 'Отзывы'
+    },
+    {
+      link: '/confidntiality',
+      title: 'Политика конфиденциальности'
+    }
+  ];
+
+  public categoriesLinks: any[] = [
+    {
+      link: '/categories/iphone',
+      title: 'iPhone'
+    },
+    {
+      link: '/categories/ipad',
+      title: 'iPad'
+    },
+    {
+      link: '/categories/macbook',
+      title: 'Macbook и iMac'
+    },
+    {
+      link: '/categories/watch',
+      title: 'Watch'
+    },
+    {
+      link: '/categories/acessories',
+      title: 'Аксессуары'
+    },
+    {
+      link: '/sales',
+      title: 'Акции'
+    }
+  ];
 }
