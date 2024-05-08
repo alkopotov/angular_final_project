9d7ad0f5440b111903c0b28dae8e5d037bad6b1e
Ilya Kotovskiy <ilya.kot.web@gmail.com>
2024-05-08 20:36:12 +0300

    add Refund Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { RefundPageComponent } from './pages/refund-page/refund-page.component';
 
 export const routes: Routes = [ 
   { path: '', component: MainPageComponent },
+  { path: 'returns', component: RefundPageComponent },
   { path: '**', component: NotFoundPageComponent } 
 ];
---
+++ src/app/pages/refund-page/refund-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-refund-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './refund-page.component.html',
+  styleUrl: './refund-page.component.css'
+})
+export class RefundPageComponent {
+
+}
