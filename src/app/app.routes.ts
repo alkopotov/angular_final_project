0c2f5dc66ca59b4e25792ba22ca9e538cf66248b
AlHobb <118611357+alkopotov@users.noreply.github.com>
2024-05-11 16:15:29 +0300

    Contacts Page Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { RefundPageComponent } from './pages/refund-page/refund-page.component';
+import { ContactsPageComponent } from './pages/contacts-page/contacts-page.component';
 
 export const routes: Routes = [ 
   { path: '', component: MainPageComponent },
   { path: 'returns', component: RefundPageComponent },
+  { path: 'contacts', component:ContactsPageComponent },
   { path: '**', component: NotFoundPageComponent } 
 ];
---
+++ src/app/pages/contacts-page/contacts-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './contacts-page.component.html',
+  styleUrl: './contacts-page.component.css'
+})
+export class ContactsPageComponent {
+
+}
