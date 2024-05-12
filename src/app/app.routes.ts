a5b7c1835df2c242bf77f2a2419d627c88659133
Ilya Kotovskiy <ilya.kot.web@gmail.com>
2024-05-12 23:43:48 +0300

    Add Delivery Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { RefundPageComponent } from './pages/refund-page/refund-page.component';
+import { DeliveryPageComponent } from './pages/delivery-page/delivery-page.component';
 
 export const routes: Routes = [ 
   { path: '', component: MainPageComponent },
   { path: 'returns', component: RefundPageComponent },
+  { path: 'delivery', component: DeliveryPageComponent },
   { path: '**', component: NotFoundPageComponent } 
 ];
