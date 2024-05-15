ac069f40f5c807fa9a26509fa6c53e7f3584c6ed
AlHobb <118611357+alkopotov@users.noreply.github.com>
2024-05-15 17:10:11 +0300

    Dialog Credit desktop added

--- src/app/services/dialog-dispatcher.service.ts
+++ src/app/services/dialog-dispatcher.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Dialog } from '@angular/cdk/dialog';
 import { DialogCallbackComponent } from '../components/dialog-callback/dialog-callback.component';
 import { DialogBasketComponent } from '../components/dialog-basket/dialog-basket.component';
+import { DialogCreditComponent } from '../components/dialog-credit/dialog-credit.component';
 
 
 export type DialogWindowType = 'basket' | 'credit' | 'callback' | 'catalog' | 'addToCart' | 'oneClick';
@@ -19,7 +20,7 @@ export class DialogDispatcherService {
 
   public dialogWindows: Record<DialogWindowType, any> = {
     basket: DialogBasketComponent,
-    credit: null,
+    credit: DialogCreditComponent,
     callback: DialogCallbackComponent,
     catalog: null,
     addToCart: null,
