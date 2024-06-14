d9c5f3f968670f262b6c68a43f999f1f35f0f0d7
AlHobb <118611357+alkopotov@users.noreply.github.com>
2024-06-14 16:33:16 +0300

    Checkboxes in accordion

--- src/app/components/filter-elem/filter-elem.component.ts
+++ src/app/components/filter-elem/filter-elem.component.ts
@@ -2,16 +2,14 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatSliderModule } from '@angular/material/slider';
 import { FilterService } from '../../services/filter.service';
-import { DecimalPipe } from '@angular/common';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatAccordion, MatExpansionModule} from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 
 
 @Component({
   selector: 'app-filter-elem',
   standalone: true,
-  imports: [MatSliderModule, FormsModule, DecimalPipe, MatCheckboxModule, MatAccordion, MatExpansionModule, MatFormFieldModule],
+  imports: [MatSliderModule, FormsModule, MatFormFieldModule, CdkAccordionModule],
   templateUrl: './filter-elem.component.html',
   styleUrl: './filter-elem.component.css'
 })
